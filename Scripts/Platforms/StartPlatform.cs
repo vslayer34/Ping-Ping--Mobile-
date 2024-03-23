92fcd46159f703a3608799bf3e3fbaa2fcec81ca
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-03-23 10:41:18 +0200

    added a test for physics body 2d InputEvent signal

--- Scripts/Platforms/StartPlatform.cs
+++ Scripts/Platforms/StartPlatform.cs
@@ -1,23 +1,49 @@
 using Godot;
-using System;
-using System.Reflection;
 
 public partial class StartPlatform : AnimatableBody2D
 {
+    [ExportGroup("Required Nodes")]
+    [Export]
+    private CollisionShape2D _collisionShape;
+    private int _collisionShapeIndex;
+
     private bool _isDragged;
 
 
     //---------------------------------------------------------------------------------------------
 
-    public override void _Input(InputEvent @event)
+    public override void _Ready()
     {
-        base._Input(@event);
+        base._Ready();
+        InputEvent += HandlePlayerTouch;
+        _collisionShapeIndex = _collisionShape.GetIndex();
+    }
+
+    private void HandlePlayerTouch(Node viewport, InputEvent @event, long shapeIdx)
+    {
         if (@event is InputEventScreenTouch touch)
         {
             if (touch.Pressed)
             {
-                GD.Print("touch happened");
+                GD.Print("Touched inside the sprite");
+            }
+            else
+            {
+                GD.Print("Touched outside");
             }
         }
     }
+
+
+    // public override void _Input(InputEvent @event)
+    // {
+    //     base._Input(@event);
+    //     if (@event is InputEventScreenTouch touch)
+    //     {
+    //         if (touch.Pressed)
+    //         {
+    //             GD.Print("touch happened");
+    //         }
+    //     }
+    // }
 }
