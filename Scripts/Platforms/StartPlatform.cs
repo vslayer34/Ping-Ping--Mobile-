914b1ccff92214402071ea97cfac16fc3d1087da
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-03-23 08:47:43 +0200

    Added a test for the touch input

--- Scripts/Platforms/StartPlatform.cs
+++ Scripts/Platforms/StartPlatform.cs
@@ -4,8 +4,20 @@ using System.Reflection;
 
 public partial class StartPlatform : CharacterBody2D
 {
+    private bool _isDragged;
+
+
+    //---------------------------------------------------------------------------------------------
+
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
+        if (@event is InputEventScreenTouch touch)
+        {
+            if (touch.Pressed)
+            {
+                GD.Print("touch happened");
+            }
+        }
     }
 }
