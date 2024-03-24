06e3a667e623755c56ddf692e4cedc8fe3eaa7e7
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-03-24 03:11:25 +0200

    Moved the pkatform horizontally and got referance to the the drage relative vector

--- Scripts/Platforms/StartPlatform.cs
+++ Scripts/Platforms/StartPlatform.cs
@@ -6,8 +6,15 @@ public partial class StartPlatform : AnimatableBody2D
     [Export]
     private CollisionShape2D _collisionShape;
     private int _collisionShapeIndex;
+    [ExportGroup("")]
 
     private bool _isDragged;
+
+    [ExportCategory("Platform properties")]
+    [Export]
+    private float _speed = 10.0f;
+
+    private Vector2 _touchPosition;
 
 
     //---------------------------------------------------------------------------------------------
@@ -19,6 +26,21 @@ public partial class StartPlatform : AnimatableBody2D
         _collisionShapeIndex = _collisionShape.GetIndex();
     }
 
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveSideWays((float) delta);
+        GD.Print(_touchPosition);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (_isDragged && @event is InputEventScreenDrag screenDrag)
+        {
+            _touchPosition = screenDrag.Relative;
+        }
+    }
+
     private void HandlePlayerTouch(Node viewport, InputEvent @event, long shapeIdx)
     {
         if (@event is InputEventScreenTouch touch)
@@ -26,24 +48,25 @@ public partial class StartPlatform : AnimatableBody2D
             if (touch.Pressed)
             {
                 GD.Print("Touched inside the sprite");
+                _isDragged = true;
+                _touchPosition = touch.Position;
             }
             else
             {
                 GD.Print("Touched outside");
+                _isDragged = false;
             }
         }
     }
 
+    private void MoveSideWays(float delta)
+    {
+        Translate(Vector2.Left * _speed * delta);
+    }
 
-    // public override void _Input(InputEvent @event)
-    // {
-    //     base._Input(@event);
-    //     if (@event is InputEventScreenTouch touch)
-    //     {
-    //         if (touch.Pressed)
-    //         {
-    //             GD.Print("touch happened");
-    //         }
-    //     }
-    // }
+
+    private void MoveHorizontally()
+    {
+
+    }
 }
