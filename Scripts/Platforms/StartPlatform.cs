4273339791bca0b92b11ba4b25e314b74d52b92c
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-03-29 06:25:15 +0200

    Trying to detect the finger removed from the screen

--- Scripts/Platforms/StartPlatform.cs
+++ Scripts/Platforms/StartPlatform.cs
@@ -29,18 +29,33 @@ public partial class StartPlatform : AnimatableBody2D
     public override void _PhysicsProcess(double delta)
     {
         MoveSideWays((float) delta);
-        GD.Print(_touchPosition);
+        // GD.Print(_touchPosition);
     }
 
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        if (_isDragged && @event is InputEventScreenDrag screenDrag)
+
+        if (@event is InputEventScreenDrag screenDrag)
         {
-            _touchPosition = screenDrag.Relative;
+            if (_isDragged)
+            {
+                _touchPosition = screenDrag.Relative;
+                GD.Print($"Is Dragged: {_isDragged}");
+            }
+
+            else if (!screenDrag.IsCanceled())
+            {
+                GD.Print("Platform is released");
+            }
         }
     }
 
+
+    /// <summary>
+    /// Handle the player touch input
+    /// Indicata when the player is touching or releasing the platform
+    /// </summary>
     private void HandlePlayerTouch(Node viewport, InputEvent @event, long shapeIdx)
     {
         if (@event is InputEventScreenTouch touch)
@@ -49,16 +64,23 @@ public partial class StartPlatform : AnimatableBody2D
             {
                 GD.Print("Touched inside the sprite");
                 _isDragged = true;
-                _touchPosition = touch.Position;
+                GD.Print($"Is Dragged: {_isDragged}");
+                // _touchPosition = touch.Position;
             }
-            else
-            {
-                GD.Print("Touched outside");
-                _isDragged = false;
-            }
+            // else
+            // {
+            //     GD.Print("Released");
+            //     _isDragged = false;
+            //     GD.Print($"Is Dragged: {_isDragged}");
+            // }
         }
     }
 
+
+    /// <summary>
+    /// Move to the left continuasly
+    /// </summary>
+    /// <param name="delta"></param>
     private void MoveSideWays(float delta)
     {
         Translate(Vector2.Left * _speed * delta);
