45cb3710f607eac6237c29665fe7729106a256c5
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-04-12 10:52:44 +0200

    Spawned the start platform at the start of the game

--- Scripts/Managers/PlarformSpawner.cs
+++ Scripts/Managers/PlarformSpawner.cs
@@ -11,6 +11,12 @@ public partial class PlarformSpawner : Node2D
     private PackedScene _newPlatform;
 
     [Export]
+    private PackedScene _newStartPlatform;
+
+    [Export]
+    private Marker2D _startPlatformSpawnPoint;
+
+    [Export]
     private GameManager _gameManager;
 
 
@@ -24,6 +30,11 @@ public partial class PlarformSpawner : Node2D
     private float _maxWaitTime;
 
 
+    //---------------------------------------------------------------------------------------------
+
+    private Vector2 _viewportSize;
+
+
 
     //---------------------------------------------------------------------------------------------
 
@@ -33,6 +44,9 @@ public partial class PlarformSpawner : Node2D
     public override void _Ready()
     {
         _timer.Timeout += SpawnNewPlatform;
+        _viewportSize = GetViewportRect().Size;
+
+        SpawnStartPlatform();
     }
 
 
@@ -51,5 +65,21 @@ public partial class PlarformSpawner : Node2D
         GD.Print("newly created platform position" + _newlyCreatedPlatform.Position);
         GD.Print("Spawner position: " + Position);
         GD.Print("");
+    }
+
+
+
+    /// <summary>
+    /// Spawn a new start platform to the scene
+    /// </summary>
+    private void SpawnStartPlatform()
+    {
+        _startPlatformSpawnPoint.GlobalPosition = _viewportSize / 2.0f;
+
+        StartPlatform startPlatform = _newStartPlatform.Instantiate<StartPlatform>();
+        _startPlatformSpawnPoint.AddChild(startPlatform);
+
+        startPlatform.Position = _startPlatformSpawnPoint.GlobalPosition;
+        GD.Print($"Start platform spawn position: {startPlatform.GlobalPosition}");
     }
 }
