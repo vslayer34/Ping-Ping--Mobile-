000c94c48a920768068644d2d61d01e9fdc66667
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-04-13 11:11:12 +0200

    Added platform destroyer with adjustable shape and location depending on  the view port

--- Scripts/Managers/PlatformDestroyer.cs
+++ Scripts/Managers/PlatformDestroyer.cs
@@ -4,10 +4,34 @@ using System;
 public partial class PlatformDestroyer : Area2D
 {
     private GameManager _gameManager;
+    private CollisionShape2D _collider;
 
     public override void _Ready()
     {
         _gameManager = GetOwner<GameManager>();
-        GD.Print(_gameManager.ViewPortSize);
+        _collider = GetNode<CollisionShape2D>("CollisionShape2D");
+
+        SetUpColliderPosition();
+    }
+
+
+    /// <summary>
+    /// Set the position of the collider outside of the viewport to the left
+    /// </summary>
+    private void SetUpColliderPosition()
+    {
+        SetUpRectangleSize();
+        _collider.Position = new Vector2(-50.0f, _gameManager.ViewPortSize.Y / 2);
+    }
+
+
+    /// <summary>
+    /// Set the rectancle size of the collider
+    /// </summary>
+    private void SetUpRectangleSize()
+    {
+        float rectancleWidth = 50.0f;
+        var rectancleShape = _collider.Shape as RectangleShape2D;
+        rectancleShape.Size = new Vector2(rectancleWidth, _gameManager.ViewPortSize.Y);
     }
 }
